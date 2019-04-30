f2ce2a0f5621d7b4805b8e6d634c476163f499b8
Fireblade75 <arnoldaverink@kamk.fi>
2019-04-30 14:43:40 +0300

    login options

--- src/client/comps/pages/login.vue
+++ src/client/comps/pages/login.vue
@@ -5,18 +5,18 @@
             <div class="form-group row">
                 <label for="login-email" class="col-sm-2 col-form-label">Email address</label>
                 <div class="col-sm-10">
-                    <input type="email" class="form-control" id="login-email" placeholder="name@example.com">
+                    <input type="email" class="form-control" id="login-email" v-model="username" placeholder="name@example.com">
                 </div>
             </div>
             <div class="form-group row">
                 <label for="login-password" class="col-sm-2 col-form-label">Password</label>
                 <div class="col-sm-10">
-                    <input type="password" class="form-control" id="login-password" placeholder="password">
+                    <input type="password" class="form-control" id="login-password" v-model="password" placeholder="password">
                 </div>
             </div>
             <div class="row">
                 <div class="offset-sm-2 col-sm-10">
-                    <button type="submit" class="btn btn-primary col-sm-3">Login</button>
+                    <button type="submit"  v-on:click="submit" class="btn btn-primary col-sm-3">Login</button>
                 </div>
             </div>
         </form>
@@ -26,6 +26,18 @@
 <script>
     export default {
         name: "Login",
+        data: {
+            username: '',
+            password: ''
+        },
+        methods: {
+            submit: function(event) {
+                console.log ({
+                    username: this.username,
+                    password: this.password
+                })
+            }
+        }
     }
 </script>
 
